7b03a962043aa67245f30534dba48f614fa8833e
kevin marin <kevinmg253@gmail.com>
2025-10-22 17:32:10 -0500

    se agrega el proyecto de js y unos ejemplos

---
+++ src/proyecto.js
@@ -0,0 +1,43 @@
+// const nombre = "Carolina";
+// const notas = [3.5, 2.0, 2.8];
+
+// const promedio = (notas[0] + notas[1] + notas[2]) / 3;
+
+// console.log(`Aprendiz: ${nombre}`);
+// console.log(`Promedio: ${promedio.toFixed(2)}`);
+// console.log(`Estado: ${promedio >= 3.5 ? "Aprobado" : "No aprobado"}`);
+
+
+// const aprendiz = {
+// nombre: "Carolina",
+// apellido: "Gómez",
+// ficha: 3223874,
+// programa: "ADSO",
+// activo: true
+// };
+// // Acceder a propiedades
+// console.log(aprendiz.nombre);
+// console.log(aprendiz["ficha"]);
+
+// const aprendices = [
+// "Carolina",
+// "Gustavo",
+// "Matías",
+// "Lina"
+// ];
+// // Acceder por índice (empieza en 0)
+// console.log(aprendices[0]); //
+
+// // Propiedades útiles
+// console.log(aprendices.length); 
+// // Agregar elementos
+// aprendices.push("Andrea");
+// aprendices.unshift("Pedro");
+
+// console.log(aprendices);
+
+
+const Numeros = [1, 2, 3, 4, 5];
+
+const dobles = Numeros.map(n => n * 2);
+console.log(dobles);
