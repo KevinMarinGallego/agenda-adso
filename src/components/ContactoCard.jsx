c990c689df3834a2538722304c8587d3be7f8313
kevin marin <kevinmg253@gmail.com>
2025-11-04 20:04:37 -0500

    Clase_5_Agenda_ADSO_v3_UI

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,31 +1,19 @@
-// Este componente muestra UN contacto de la agenda.
-// Recibe los datos (props): nombre, telefono, correo, etiqueta.
-
 export default function ContactoCard({
-  id,
   nombre,
   telefono,
   correo,
   etiqueta,
-  onDelete,
+  onEliminar,
 }) {
   return (
-    <article className="tarjeta-contacto">
-      <h3>{nombre} </h3>
-      {etiqueta && <p className="tag">{etiqueta}</p>}
-      <p>📞 {telefono}</p>
-      {correo && <p>✉️ {correo}</p>}
-      
-
-      <div className="acciones">
-        <button
-          type="button"
-          className="btn-eliminar"
-          onClick={() => onDelete(id)}
-        >
-          Eliminar
-        </button>
-      </div>
-    </article>
+    <div className="card">
+      <h3>{nombre}</h3>
+      <p>📱 {telefono}</p>
+      <p>✉️ {correo}</p>
+      {etiqueta && <span className="tag">{etiqueta}</span>}
+      <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
+        Eliminar
+      </button>
+    </div>
   );
 }
